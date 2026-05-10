10ae875e3d746f83f67a06d3eb306d8217238104
Shruti Singh <shruti.25nov.8@gmail.com>
2026-05-10 16:34:57 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,29 @@
+# ans.1   a = 2
+# b = 5
+# c = a + b
+# c = 2 + 5
+# c = 7
+
+# ans 2         A <10  and A > B
+# 5 < 10 ans 5 > 3
+# true 
+
+# ans 3        1. False     2.false     3. true
+
+# ans 4
+# N = 5678
+#s = N // 1000
+#print(s)
+
+# ans 5
+# P = 3928
+#d = P % 10
+#print(d)
+
+# 6. 9 % 2 
+# remainder = 1
+
+# 7. 2*5 = 10
+
+# 8. 50+20+100 = 170 
+# 170 / 3 
