fcff3aa6cbd2df44eb8378362464469724620ed5
Shruti Singh <shruti.25nov.8@gmail.com>
2026-05-12 16:51:59 +0530

    Add files via upload

---
+++ decision making.py
@@ -0,0 +1,64 @@
+# ans 1.
+# marks = 1
+#if marks >= 40 and marks <= 50:
+ #   print("A")
+#elif marks >= 30 and marks <= 40:
+ #   print("B")    
+#elif marks >= 20 and marks <= 30:
+ #   print("C") 
+#elif marks >= 10 and marks <= 20:
+ #   print("D") 
+#elif marks >= 0 and marks <= 10:
+ #   print("E") 
+#else:
+ #   print("Invalid marks")    
+
+#ans 2
+# char = 'E'
+# if char =='P' or char == 'p':
+#    print("PrepBytes")
+# elif char =='Z' or char == 'z':
+#    print("Zenith")
+# elif char =='E' or char == 'e':
+#    print("Expert Coder")        
+# elif char =='D' or char == 'd':
+#    print("Data Structure")
+# else:
+#    print("Invalid character") 
+
+#ans 3
+
+# A = 2
+# B = 5
+# C = 4
+
+# if A == B and B == C:
+#     print(-1)
+# elif A > B and B > C:
+#     print(A)
+# elif B > A and B > C:
+#     print(B)
+# else:
+#     print(C)            
+
+
+# ans 4.
+# X = 2
+# Y = 9
+# Z = 23
+# if  X > Y and X < Z :
+#     print(X)
+# elif Y > X and Y < Z:
+#     print(Y)    
+# else:
+#     print(Z)       
+
+# ans 5
+# A = 60
+# B = 100
+# C = 20
+
+# if A > 90 or B > 90 or C > 90:
+#      print("Obtuse")
+# else:
+#      print("Acute")    
